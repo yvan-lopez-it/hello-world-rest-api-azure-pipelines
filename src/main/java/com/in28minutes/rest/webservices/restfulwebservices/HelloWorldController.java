7b2567f0eb71c68e4dc0e0aad8e51677e797d58d
Yvan Lopez <yvan.lopez.it@gmail.com>
2023-04-18 10:25:37 -0500

    V11 Change

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -16,7 +16,7 @@ public class HelloWorldController {
 	public HelloWorldBean helloWorldBean() {
 		// throw new RuntimeException("Some Error has Happened! Contact Support at
 		// ***-***");
-		return new HelloWorldBean("Hello World");
+		return new HelloWorldBean("Hello World V11");
 	}
 
 	/// hello-world/path-variable/in28minutes
